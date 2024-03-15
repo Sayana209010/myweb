a5bb4ba98881795c8d3bf595e1ebf5c142834e32
Sayana <163549624+Sayana209010@users.noreply.github.com>
2024-03-15 17:25:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('#enter');
+const login = document.querySelector('#login');
+const pass = document.querySelector('#pass');
+
+const rightLogin = "Test"
+const rightPass = "109030"
+function checkLogin() {
+  if (login.value == "Test" && pass.value == "109030") {
+    alert('Hi!')
+    document.querySelector("#Bulb").style.display = "block"
+  }
+else {
+  alert('Bye!')
+  }
+}
+
+
+button.addEventListener('click', checkLogin)
